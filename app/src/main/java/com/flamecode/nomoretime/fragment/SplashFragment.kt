ae862483a4fe9e5eb9d7ac213884b35b19524afe
Okiul <ochian_a@yahoo.ro>
2021-03-27 13:40:52 +0200

    Added pulse animation and made splash screen

--- app/src/main/java/com/flamecode/nomoretime/fragment/SplashFragment.kt
+++ app/src/main/java/com/flamecode/nomoretime/fragment/SplashFragment.kt
@@ -1,13 +1,20 @@
 package com.flamecode.nomoretime.fragment
 
+import android.media.Image
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import android.widget.TextView
 import com.flamecode.nomoretime.R
 
 class SplashFragment : Fragment() {
+
+    private lateinit var pulseAnimation : Animation
 
 
     override fun onCreateView(
@@ -15,6 +22,18 @@ class SplashFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_splash, container, false)
+        val view =  inflater.inflate(R.layout.fragment_splash, container, false)
+
+        getData(view)
+
+        return view
+    }
+
+    private fun getData(view: View) {
+        var appImage  = view.findViewById<ImageView>(R.id.app_icon)
+        var appName = view.findViewById<TextView>(R.id.app_name)
+        val pulseAnimation : Animation = AnimationUtils.loadAnimation(context, R.anim.pulse)
+        appImage.startAnimation(pulseAnimation)
+        appImage.startAnimation(pulseAnimation)
     }
 }
