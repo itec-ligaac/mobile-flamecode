23db77679b596b26d36b66027122c2691b4e44e9
Iomava <imac.rares@gmail.com>
2021-03-27 18:23:48 +0200

    resolve issue HERE SDK

--- app/src/main/java/com/flamecode/nomoretime/MainActivity.kt
+++ app/src/main/java/com/flamecode/nomoretime/MainActivity.kt
@@ -1,10 +1,21 @@
 package com.flamecode.nomoretime
 
+import android.content.pm.ApplicationInfo
+import android.content.pm.PackageManager
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.flamecode.nomoretime.fragment.SplashFragment
 import com.flamecode.nomoretime.manager.FragmentManager
+import com.here.android.mpa.common.ApplicationContext
+import com.here.android.mpa.common.MapEngine
+import com.here.android.mpa.common.MapSettings
+import com.here.android.mpa.common.OnEngineInitListener
+import java.io.File
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,5 +38,42 @@ class MainActivity : AppCompatActivity() {
     private fun openSplashFragment() {
 
         FragmentManager(supportFragmentManager).moveToNextFragment(SplashFragment())
+    }
+
+    private fun initMapEngine() {
+        val diskCacheRoot: String = Environment.getExternalStorageDirectory()
+            .getPath() + File.separator.toString() + ".isolated-here-maps"
+        var intentName = ""
+        try {
+            val ai: ApplicationInfo = this.packageManager
+                .getApplicationInfo(this.packageName, PackageManager.GET_META_DATA)
+            val bundle = ai.metaData
+            intentName = bundle.getString("INTENT_NAME")!!
+        } catch (e: PackageManager.NameNotFoundException) {
+            Log.e(
+                this.javaClass.toString(),
+                "Failed to find intent name, NameNotFound: " + e.message
+            )
+        }
+        val success = MapSettings.setIsolatedDiskCacheRootPath(diskCacheRoot)
+        if (!success) {
+        } else {
+            MapEngine.getInstance().init(
+                ApplicationContext(this)
+            ) { error -> //Here is
+                Log.e(
+                    "TAG",
+                    "Map Engine initialized with error code: $error"
+                )
+                Toast.makeText(
+                    this,
+                    "Map Engine initialized with error code:$error",
+                    Toast.LENGTH_SHORT
+                ).show()
+                if (error == OnEngineInitListener.Error.NONE) {
+                   // getCoordinatesForAddress(mAddress)
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/flamecode/nomoretime/fragment/HomeFragment.kt
+++ app/src/main/java/com/flamecode/nomoretime/fragment/HomeFragment.kt
@@ -1,13 +1,24 @@
 package com.flamecode.nomoretime.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 import com.flamecode.nomoretime.R
+import com.here.android.mpa.common.ApplicationContext
+import com.here.android.mpa.common.GeoCoordinate
+import com.here.android.mpa.common.MapEngine
+import com.here.android.mpa.common.OnEngineInitListener
+import com.here.android.mpa.mapping.AndroidXMapFragment
+import com.here.android.mpa.mapping.Map
+
 
 class HomeFragment : Fragment() {
+
+    private var map : Map = Map()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
