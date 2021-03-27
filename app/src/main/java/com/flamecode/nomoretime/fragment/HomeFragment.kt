73166e43399d48013ce90727e5bbe55bda264d50
Iomava <imac.rares@gmail.com>
2021-03-27 18:51:56 +0200

    resolve issue HERE SDK #2

--- app/src/main/java/com/flamecode/nomoretime/fragment/HomeFragment.kt
+++ app/src/main/java/com/flamecode/nomoretime/fragment/HomeFragment.kt
@@ -5,12 +5,11 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 import com.flamecode.nomoretime.R
 import com.here.android.mpa.common.ApplicationContext
-import com.here.android.mpa.common.GeoCoordinate
-import com.here.android.mpa.common.MapEngine
 import com.here.android.mpa.common.OnEngineInitListener
 import com.here.android.mpa.mapping.AndroidXMapFragment
 import com.here.android.mpa.mapping.Map
@@ -18,7 +17,7 @@ import com.here.android.mpa.mapping.Map
 
 class HomeFragment : Fragment() {
 
-    private var map : Map = Map()
+    private var map: Map? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,7 +26,20 @@ class HomeFragment : Fragment() {
 
         val view = inflater.inflate(R.layout.fragment_home, container, false)
 
+        val mapFragment = AndroidXMapFragment()
+        val fm: FragmentManager = fragmentManager!!
+        val ft: FragmentTransaction = fm.beginTransaction()
+        ft.replace(R.id.simpleFrameLayout, mapFragment).commit()
 
+        mapFragment.init(ApplicationContext(context!!), OnEngineInitListener { error ->
+            if (error == OnEngineInitListener.Error.NONE) {
+
+                // retrieve a reference of the map from the map fragment
+                map = mapFragment.map
+            } else {
+                Log.e("TAG","ERROR: Cannot initialize Map Fragment: " + error.stackTrace)
+            }
+        })
 
         return view
     }
