0ef5e42aabc41e78c34e4f2f65412379c83d9b69
Iomava <imac.rares@gmail.com>
2021-03-27 14:31:04 +0200

    Change color navigation bar

--- app/src/main/java/com/flamecode/nomoretime/MainActivity.kt
+++ app/src/main/java/com/flamecode/nomoretime/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.flamecode.nomoretime
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.flamecode.nomoretime.fragment.SplashFragment
@@ -11,9 +12,20 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         openSplashFragment()
+        setColorForNavigationBar()
+    }
+
+    private fun setColorForNavigationBar() {
+
+        // set the bottom bar a new color
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+
+            window.navigationBarColor = resources.getColor(R.color.black, theme)
+        }
     }
 
     private fun openSplashFragment() {
+
         FragmentManager(supportFragmentManager).moveToNextFragment(SplashFragment())
     }
 }
